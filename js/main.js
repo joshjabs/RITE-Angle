17a50cf835c26ddebd905ec5ad1ad27c692546c1
Blake Stevens <bwstevens@mavs.coloradomesa.edu>
2017-02-27 09:14:22 -0700

    stop tun key up

--- js/main.js
+++ js/main.js
@@ -1,64 +1,108 @@
-//initiate the Phaser framework
-var game = new Phaser.Game(592, 360, Phaser.AUTO);
+    //initiate the Phaser framework
+    var controls = {};
+    var game = new Phaser.Game(800, 800, Phaser.AUTO); 
 
-var GameState = {
-    
-//    //initiate game settings
-//    init: function (){
-//        
-//    },
-    
-    //load the game assets before the game starts
-    preload: function (){
-        this.load.image('platform', 'assets/images/Platform1.png');
-//    this.load.image('triangle', 'assets/images/triangle1.png');
-//    this.load.image('circle', 'assets/images/circle1.png');    
-         game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.physics.arcade.gravity.y = 1000;
+    var GameState = {
 
-    },
-    
-    //execute after everything is loaded
-    create: function (){
-      game.stage.backgroundColor = "#ffffff"
-     
+
+
+      //initiate game settings
+      init: function() {},
+
+      //load the game assets before the game starts
+      preload: function() {
+        this.game.load.spritesheet('player', 'assets/therun.png', 280, 385);
+        this.game.load.tilemap('tilemap', 'assets/level.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/tiles_spritesheet.png');
+      },
+
+      //execute after everything is loaded
+      create: function() {
+
+        controls = {
+          right : this.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+          left : this.input.keyboard.addKey(Phaser.Keyboard.LEFT),
+          up : this.input.keyboard.addKey(Phaser.Keyboard.UP),
+        };
+
+
+        //Start the Arcade Physics systems
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //Change the background colour
+        this.game.stage.backgroundColor = "#a9f0ff";
+
+        //Add the tilemap and tileset image. The first parameter in addTilesetImage
+        //is the name you gave the tilesheet when importing it into Tiled, the second
+        //is the key to the asset in Phaser
+        this.map = this.game.add.tilemap('tilemap');
+        this.map.addTilesetImage('tiles128', 'tiles');
+
+        //Add both the background and ground layers. We won't be doing anything with the
+        //GroundLayer though
+        this.backgroundlayer = this.map.createLayer('BackgroundLayer');
+        this.groundLayer = this.map.createLayer('GroundLayer');
+
+        //Before you can use the collide function you need to set what tiles can collide
+        this.map.setCollisionBetween(1, 100, true, 'GroundLayer');
+
+        //Add the sprite to the game and enable arcade physics on it
+        this.sprite = this.game.add.sprite(50, 50, 'player');
         
-      this.platform1 = this.game.add.sprite(148, 100, 'platform');
-         this.game.physics.arcade.enable(this.platform1);
-         this.platform1.body.allowGravity = true;
-        
-        
-      this.platform2 = this.game.add.sprite(148, 0, 'platform');
-        this.game.physics.arcade.enable(this.platform2);
-        this.platform2.body.allowGravity = true;
-        
-//      this.circle1 = this.game.add.sprite(350, 300, 'circle')
-//      this.circle1.anchor.setTo(0.5, 0.5);
-//      this.circle1.scale.setTo(0.3);
-//        
-//      this.triangle1 = this.game.add.sprite(225, 200, 'triangle');
-//      this.triangle1.anchor.setTo(0.5, 0.5);
-//      this.triangle1.scale.setTo(1, 0.8);
-//      this.triangle1.angle = -55;
-        
-      this.platform3 = this.game.add.sprite(325, 250, 'platform');
-      this.platform3.anchor.setTo(0.5, 0.5);
-//      this.platform3.scale.setTo(0.4, 1);
-//      this.platform3.angle = 10;
-         this.game.physics.arcade.enable(this.platform3);
+        this.game.physics.arcade.enable(this.sprite);
+
+        //Change the world size to match the size of this layer
+        this.groundLayer.resizeWorld();
+
+        //Set some physics on the sprite
+        this.sprite.body.bounce.y = 0.2;
+        this.sprite.body.gravity.y = 2000;
+        this.sprite.body.gravity.x = 20;
+        this.sprite.body.velocity.x = 0;
           
-         this.platform3.body.immovable = false;
-      
-        
-      this.startPlatform = this.game.add.sprite(100, 190, 'platform');
-      this.startPlatform.angle = 90;
-      this.startPlatform.scale.setTo(0.5, 5);
-    },
-    
-    update: function (){
-        
-    }
-};
+        //Create a running animation for the sprite and play it
+        this.sprite.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
+//      this.sprite.animations.play('right');
 
-game.state.add('GameState', GameState);
-game.state.start('GameState');
+
+        //Make the camera follow the sprite
+        this.game.camera.follow(this.sprite);
+
+        //Enable cursor keys so we can create some controls
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+
+
+      },
+
+      update: function() {
+
+        //Make the sprite collide with the ground layer
+        this.game.physics.arcade.collide(this.sprite, this.groundLayer);         
+
+        if(controls.up.isDown){
+            this.sprite.body.velocity.y = -600;
+
+        }
+
+        if(this.cursors.right.isDown){
+//          this.sprite.body.bounce.y = 0.2;
+            this.sprite.animations.play('right');
+            this.sprite.body.velocity.x = 850;
+        }
+          
+        if(this.cursors.right.isUp){
+            this.sprite.animations.stop(null, true);
+            this.sprite.body.velocity.x = 0;
+        }
+          
+
+        if(controls.left.isDown){
+          this.sprite.body.bounce.y = 0.2;
+        }
+
+
+     }
+    };
+
+    game.state.add('GameState', GameState);
+    game.state.start('GameState');
